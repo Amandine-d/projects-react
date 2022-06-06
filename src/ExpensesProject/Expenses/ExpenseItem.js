abb458c2750ed6438e979d83ee0e807fb399b878
Amandine <amandine.druart@gmail.com>
2022-06-06 16:36:02 +0200

    Comment useless code

--- src/ExpensesProject/Expenses/ExpenseItem.js
+++ src/ExpensesProject/Expenses/ExpenseItem.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
+  // const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
+  // const clickHandler = (props) => {
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button className='expense-item__btn' onClick={clickHandler}>Change Title</button>
+      {/* <button className='expense-item__btn' onClick={clickHandler}>Change Title</button> */}
     </Card>
   )
 }
