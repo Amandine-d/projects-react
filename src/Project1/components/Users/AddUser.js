45bca2916a8c18a291adf311e9ce2d4839f0e37f
Amandine <amandine.druart@gmail.com>
2022-06-07 16:54:06 +0200

    Styling: Make sure the input is not empty

--- src/Project1/components/Users/AddUser.js
+++ src/Project1/components/Users/AddUser.js
@@ -9,6 +9,13 @@ const AddUser = props => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      //enteredage is a string here, we make sure with the + that it is a number
+      return;
+    }
     console.log(enteredUsername, enteredAge);
     setEnteredAge('');
     setEnteredAge('');
