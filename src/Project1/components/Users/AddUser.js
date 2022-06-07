0166e9b426e6131278ad9cccdcc1e7614b6a8d44
Amandine <amandine.druart@gmail.com>
2022-06-07 16:45:39 +0200

    Styling: Add button component

--- src/Project1/components/Users/AddUser.js
+++ src/Project1/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = props => {
   const addUserHandler = (event) => {
@@ -7,13 +9,13 @@ const AddUser = props => {
   }
 
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age</label>
         <input id="age" type="number" />
-        <button type="submit">Add user</button>
+        <Button type="submit">Add user</Button>
       </form>
     </Card>
   )
