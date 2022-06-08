95f5828716cb72c6d046fe98c6a87e46e49e9613
Amandine <amandine.druart@gmail.com>
2022-06-08 15:59:35 +0200

    Starting up section 10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import AppExpenses from './ExpensesProject/AppExpenses';
 // import AppStyling from "./StylingComponents/AppStyling";
-import AppProject1 from './Project1/AppProject1';
+// import AppProject1 from './Project1/AppProject1';
 import './App.css';
+import AppSection10 from './Section-10/AppSection10';
 
 function App() {
   return (
     <div className="App">
       {/* <AppExpenses /> */}
       {/* <AppStyling /> */}
-      <AppProject1 />
+      {/* <AppProject1 /> */}
+      <AppSection10 />
     </div>
   );
 }
