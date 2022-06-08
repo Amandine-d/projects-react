bb03106ee698c8752f225d6522391c47571d2fd4
Amandine <amandine.druart@gmail.com>
2022-06-08 18:10:13 +0200

    UseReducer: for checking validity of the email

--- src/Section-10/components/Login/Login.js
+++ src/Section-10/components/Login/Login.js
@@ -1,42 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+
+  }
+  if (action.type === 'ACTION_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@')};
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form valdiity');
-      setFormIsValid(
-        enteredEmail.target.value.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
+  const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: false });
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form valdiity');
+  //     setFormIsValid(
+  //       enteredEmail.target.value.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
 
-    //Clean up function. Will run as a clean up process
-    return () => {
-      console.log('Clean up');
-      clearTimeout(identifier);
+  //   //Clean up function. Will run as a clean up process
+  //   return () => {
+  //     console.log('Clean up');
+  //     clearTimeout(identifier);
 
-    };
-  }, [enteredEmail, enteredPassword]);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    // setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@')
+      && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid
+      && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: 'INPUT_BLUR' })
   };
 
   const validatePasswordHandler = () => {
@@ -45,21 +69,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
