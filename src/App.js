46a611674a301f4b480494b412815158a560ab2b
Amandine <amandine.druart@gmail.com>
2022-06-13 14:29:50 +0200

    Food order App: start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import AppExpenses from './ExpensesProject/AppExpenses';
 // import AppStyling from "./StylingComponents/AppStyling";
 // import AppProject1 from './Project1/AppProject1';
-import './App.css';
-import AppSection10 from './Section-10/AppSection10';
-import { AuthContextProvider } from './Section-10/store/auth-context';
+import FoodOrderApp from './FoodOrder/FoodOrderApp';
+// import './App.css';
+// import AppSection10 from './Section-10/AppSection10';
+// import { AuthContextProvider } from './Section-10/store/auth-context';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       {/* <AppExpenses /> */}
       {/* <AppStyling /> */}
       {/* <AppProject1 /> */}
-      <AuthContextProvider>
+      {/* <AuthContextProvider>
         <AppSection10 />
-      </AuthContextProvider>
+      </AuthContextProvider> */}
+      <FoodOrderApp />
     </div>
   );
 }
