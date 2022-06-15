96872841a58ddea0bbfc37c337605806876886a3
Amandine <amandine.druart@gmail.com>
2022-06-15 16:58:21 +0200

    Food app: use ref and context to add items

--- src/FoodOrder/components/Meals/MealItem/MealItemForm.js
+++ src/FoodOrder/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,27 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = event => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    //value is always a string we have to cenvert it
+    const enteredAmountNumber = +enteredAmount;
+    
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 ||enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+      ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
@@ -17,6 +34,7 @@ const MealItemForm = (props) => {
       />
       {/* All input props for input element */}
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
   );
 };
