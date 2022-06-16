43e0869c8cc6dfc11cddcdd6782f72c44c9c716d
Amandine <amandine.druart@gmail.com>
2022-06-16 11:17:15 +0200

    How to: UseCallback and its dependecies

--- src/HowItWorks/HowToApp.js
+++ src/HowItWorks/HowToApp.js
@@ -4,19 +4,32 @@ import Button from './components/UI/Button/Button';
 
 const HowToApp = () => {
   const [showParagraph, setShowParagraph] = useState(false);
-  
-  const toggleParagraphHandler = useCallback (() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
   //[]
   //This callback will never change, the same component will be reused on reload
   //Insure that all the props are comparable on memo
+
+  //Whn a function is defined, js locks in all the variables we are using in there : allowtoggle. 
+  //Pb [] use the old value
+  //[allowToggle] => when has a new value, we re create the function and allow us to use the new value
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div>
       <h1>Hi!</h1>
       <DemoOutput show={showParagraph} />
       {/* {showParagraph && <p>This is new</p>} */}
+      <Button onClick={allowToggleHandler}>Allow Paragraph</Button>
+
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
