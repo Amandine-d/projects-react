255a3d3e55bd98b72ff0c841d05de9421e258f65
Amandine <amandine.druart@gmail.com>
2022-06-16 11:45:42 +0200

    How to: Optimize with useMemo

--- src/HowItWorks/HowToApp.js
+++ src/HowItWorks/HowToApp.js
@@ -1,10 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
+import DemoList from './components//Demo/DemoList';
 import DemoOutput from "./components/Demo/DemoOutput";
 import Button from './components/UI/Button/Button';
 
 const HowToApp = () => {
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
+
+  const [listTitle, setListTitle] = useState('My List');
 
   const toggleParagraphHandler = useCallback(() => {
     if (allowToggle) {
@@ -15,13 +18,22 @@ const HowToApp = () => {
   //This callback will never change, the same component will be reused on reload
   //Insure that all the props are comparable on memo
 
-  //Whn a function is defined, js locks in all the variables we are using in there : allowtoggle. 
+  //Whn a function is defined, js locks in all the variables we are using in there : allowtoggle.
   //Pb [] use the old value
   //[allowToggle] => when has a new value, we re create the function and allow us to use the new value
 
   const allowToggleHandler = () => {
     setAllowToggle(true);
   };
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+  //memoize data
+  //useCallback to memoize function
+
 
   return (
     <div>
@@ -31,6 +43,9 @@ const HowToApp = () => {
       <Button onClick={allowToggleHandler}>Allow Paragraph</Button>
 
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List title</Button>
+
     </div>
   );
 };
