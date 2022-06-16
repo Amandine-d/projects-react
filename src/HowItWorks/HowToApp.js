856ca555d5fc68975abf968f9278e9a34be05f37
Amandine <amandine.druart@gmail.com>
2022-06-16 11:08:07 +0200

    How to: UseCallback prevents function re-creation

--- src/HowItWorks/HowToApp.js
+++ src/HowItWorks/HowToApp.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import DemoOutput from "./components/Demo/DemoOutput";
 import Button from './components/UI/Button/Button';
 
 const HowToApp = () => {
   const [showParagraph, setShowParagraph] = useState(false);
-
-  const toggleParagraph = () => {
+  
+  const toggleParagraphHandler = useCallback (() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
+  //[]
+  //This callback will never change, the same component will be reused on reload
+  //Insure that all the props are comparable on memo
 
   return (
     <div>
       <h1>Hi!</h1>
       <DemoOutput show={showParagraph} />
       {/* {showParagraph && <p>This is new</p>} */}
-      <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 };
