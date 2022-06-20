90a45b1f59a3d5b69e76634dfc414301c597282b
Amandine <amandine.druart@gmail.com>
2022-06-20 14:05:47 +0200

    Hooks: Using the Http Hook and useCallback

---
+++ src/CustomHooksExample/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useCallback, useState } from "react";
+
+const useHttp = (applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, [applyData]);
+  //We should have requestConfig and applyData in the array, but bot of them are objects.
+  //So in the app comp we have to make sure they are not rendered all the time when using the function App
+  //We have to wrap transformTasks with useCallback to make sure of that
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
