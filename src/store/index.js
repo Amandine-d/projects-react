535707ed7ef77d4220d53a19b3d06e4870b5f169
Amandine <amandine.druart@gmail.com>
2022-06-24 20:41:46 +0200

    Add state slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,30 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  //we prepare a slice of our global state
+  name: 'counter',
+  initialState,
+  reducers: {
+    //map of all the reducers this state's slice needs
+    increment(state) {
+      state.counter++;
+      //here a new state will be created even if it doesn't look like it because of an internal package from redux toolkit
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+
+  }
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
@@ -9,6 +33,10 @@ const counterReducer = (state = initialState, action) => {
       showCounter: state.showCounter
     };
   };
+
+  //The object we return will over-write the existing state
+  //that's why we have to return the whole object
+  //We should always return a brand new object
 
   if (action.type === 'increase') {
     return {
