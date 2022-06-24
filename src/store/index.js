141690b6ba605a9cb35a840d4358cf2d731ab1b9
Amandine <amandine.druart@gmail.com>
2022-06-24 21:13:56 +0200

    Add an authentication state

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 // import { createStore } from 'redux';
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   //we prepare a slice of our global state
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     //map of all the reducers this state's slice needs
     increment(state) {
@@ -64,8 +64,25 @@ const counterSlice = createSlice({
 //   return state;
 // };
 
+const initialAuthState = {
+  isAuthenticated: false
+}
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer}
   //the value can be a single reducer or multiple state slices
 });
 
@@ -73,5 +90,7 @@ export const counterActions = counterSlice.actions;
 //returns an action object of this shape:
 //{type: 'some auto-generated unique identifier'}
 //action creators
+
+export const authActions = authSlice.actions;
 
 export default store;
