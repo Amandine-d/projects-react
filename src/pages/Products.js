73c32d91af76db91f899f5748be6f56e96437450
Amandine <amandine.druart@gmail.com>
2022-06-26 20:35:02 +0200

    Add NavLink

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,28 @@
+import { NavLink } from 'react-router-dom'
+
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <NavLink to='/products/p1'>
+            A book
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/products/p2'>
+            Yoga classes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/products/p3'>
+            An online course
+          </NavLink>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
