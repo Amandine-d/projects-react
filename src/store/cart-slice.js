3f7a632e29188d04b4b8a1f89d53962acab901c4
Amandine <amandine.druart@gmail.com>
2022-06-26 14:22:56 +0200

    Update total items in cart: add and remove

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-createSlice({
-  name:'cart',
+const cartSlice = createSlice({
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
@@ -11,6 +11,7 @@ createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           itemId: newItem.id,
@@ -24,6 +25,20 @@ createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {}
-  }
-})
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
