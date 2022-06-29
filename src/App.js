37c21b8d01b848f79eb997008b4e405d2ff298ee
Amandine <amandine.druart@gmail.com>
2022-06-29 10:02:59 +0200

    UseContext to render products

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Navigation from './components/Nav/Navigation';
 import ProductsPage from './containers/Products';
 import FavoritesPage from './containers/Favorites';
+import Products from './containers/Products';
+import Favorites from './containers/Favorites';
 
-const App = props => {
+const App = (props) => {
   return (
     <React.Fragment>
       <Navigation />
       <main>
-        <Route path="/" component={ProductsPage} exact />
-        <Route path="/favorites" component={FavoritesPage} />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
       </main>
     </React.Fragment>
   );
