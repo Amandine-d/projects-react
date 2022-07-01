2f7035eee28e1cc05fa0d306d5e65ae425f200f9
Amandine <amandine.druart@gmail.com>
2022-07-01 08:49:38 +0200

    Mock a request

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,9 +3,20 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    //fn create a dummy function
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{
+        id: 'p1',
+        title: 'first post'
+      }],
+    });
+    //we simulate the success case to not send unnecessary requests
+
     render(<Async />)
 
     const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
+    //Check that the list is not empty
   });
 });
